430b9dcccd3fcc439fb4c57089bf0b1d17a1b354
Reinout Claeys (laduguer) <reinout.claeys@gmail.com>
2020-05-14 14:45:56 +0200

    serial monitor implementation for ATTiny85

---
+++ laduguer/serial.h
@@ -0,0 +1,14 @@
+//
+// Created by laduguer on 13/05/2020.
+//
+
+#ifndef LADUGUER_SERIAL_H
+#define LADUGUER_SERIAL_H
+
+//
+// public methods
+//
+
+
+#endif //LADUGUER_SERIAL_H
+
---
+++ main.c
@@ -0,0 +1,24 @@
+/*  Software ("bit-bang") UART Transmitter (8 data bits, 1 stop bit, no parity)
+    for Attiny24A/44A/84A using the internal 8MHz oscillator as clock source
+
+    (c) 2018 Marcel Meyer-Garcia
+    see LICENCE.txt
+ */
+
+/* NOTE: since the internal 8MHz oscillator is not very accurate, the value for OCR0A can be tuned
+   to achieve the desired baud rate (nominal value is 103)
+*/
+
+#include "laduguer/serial.h"
+#include <util/delay.h>
+
+int main(void)
+{
+    begin();
+    while(1)
+    {
+        sendString("Hi manneke 4 \n");
+        _delay_ms(500);
+    }
+    return 0;
+}
